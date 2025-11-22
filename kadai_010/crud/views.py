3ccb425526e0fc09020d81393b8f974710690978
kiltusi <t507k507g8@gmail.com>
2025-11-22 16:57:23 +0900

    kadai_010をコミット

--- kadai_010/crud/views.py
+++ kadai_010/crud/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView,DetailView
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
 from django.urls import reverse_lazy
@@ -10,6 +11,10 @@ class TopView(TemplateView):
 class ProductListView(ListView):
     model = Product
     paginate_by = 3
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "crud/product_detail.html"
 
 class ProductCreateView(CreateView):
     model = Product
--- kadai_010/myproject/urls.py
+++ kadai_010/myproject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crud import views
+from crud.views import ProductDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,5 @@ urlpatterns = [
     path('crud/new/', views.ProductCreateView.as_view(), name="new"),
     path('crud/edit/<int:pk>', views.ProductUpdateView.as_view(), name="edit"),
     path('crud/delete/<int:pk>', views.ProductDeleteView.as_view(), name="delete"),
+     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
